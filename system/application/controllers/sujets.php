4c64ac3a698994642aaf33451821bdeb5ca26d05
Tarik Zakaria Benmerar <tarik.benmerar@acigna.com>
2014-04-22 19:56:05 +0100

    Première étape de l'intégration de la page de publication des sujetsd'examen.

--- system/application/controllers/sujets.php
+++ system/application/controllers/sujets.php
@@ -9,12 +9,14 @@ class Sujets extends Controller {
         parent::Controller();
         $this->load->database();
         $this->load->library('oms');
+        $this->load->library('generateurCode');
         $this->load->model('catalogue/specialite','specialite');
         $this->load->model('catalogue/annee','annee');
         $this->load->model( 'contenu/sujetaccepte', 'sujet' );
         
     }
  	
+    //Afficher les années d'une spécialité donnée
     function annees($idspecialite=0) {
     
         //Vérifier l'existance de la spécialité, puis la récupérer 
@@ -27,7 +29,8 @@ class Sujets extends Controller {
         $this->load->view( "contenu/annees", array( 'type'=>'sujets','nom' => $specialite->nom, 'annees'=>$annees ) );  
                    	
     }
-        
+
+    //Afficher le contenu d'une année particulière        
     function contenu($idannee=0) {
     
         //Vérifier l'existance de l'année
@@ -40,13 +43,23 @@ class Sujets extends Controller {
         $this->load->view( "contenu/sujets", array( 'nomannee'=> $annee->nom, 'sujets' => $sujets ) );
     }
 
+    //Publier un sujet d'examen
     function publier() {
-	
+        //Verfier si l'utilisateur est connecté
+        $this->oms->verifierConnecte();
+
+        //Générer la Captcha
+        $captcha = $this->generateurcode->getCaptcha();
+
+        //Récupérer les années avec leurs spécialités associées, ainsi que l'année à afficher (Par défaut ou le premier)
+        $specialites = $this->annee->listerAnneesSpec();
+    
+        //Charger les helpers requis.
+        $this->load->helper('url');
+
+        $this->load->view( 'contenu/sujet_publier', array( 'specialites' => $specialites, 
+            'captcha' => $captcha ) );	
     }
 }
-
-
-
-
 
 ?>
---
+++ system/application/views/contenu/sujet_publier.php
@@ -0,0 +1,99 @@
+<?php
+  //Charger le helper url
+  $this->load->helper('url');
+  
+  //Récupérer la libririe OMS, et afficher la partie haute
+  $this->load->library('oms');
+  $this->oms->partie_haute("Création d'un contenu pour les Sujets", array(), 
+                           array( base_url()."static/js/libs/require.js", base_url()."static/js/libs/jquery.min.js", 
+                                  base_url()."static/js/base.js"));
+?>
+
+
+<h2 class="center">Création d'un contenu pour les Sujets d'examen</h2>
+<form method="POST"  enctype="multipart/form-data" class="form">	
+  <table>
+    <tr>
+      <td class="right"><label for="NomAnnee">Année:</label></td>
+      <td class="left">
+        <select id="NomAnnee">
+        <?php
+          foreach( $specialites as $specialite ) {  
+        ?>
+          <optgroup label='<?=stripslashes($specialite['nom']); ?>'>
+          <?php
+            foreach( $specialite['annees'] as $annee ) {
+                $selected = $default['annee'] == $annee['id'] ? 'selected' : '';        
+           ?>
+            <option value='<?=$annee['id']; ?>' <?=$selected; ?>><?=stripslashes($annee['nom']); ?></option>
+           <?php
+            }
+            ?>   	
+          </optgroup>	 	       
+        <?php
+          }
+   		?>
+        </select>
+      </td>
+      <td></td>
+    </tr>
+    <tr>
+      <td class="right"><label for="NomModule">Module:</label></td>  
+      <td class="left">   
+        <select id="NomModule">  	      		
+        <?php
+          foreach( $modules as $module ) { 
+              $selected = $default['module'] == $module['id'] ? 'selected' : ''; 
+        ?>
+          <option value='<?=$module['id']; ?>' <?=$selected; ?>><?=$module['nom']; ?></option>
+        <?php
+          }
+        ?>
+        </select>
+      </td> 
+    </tr>
+    <tr>
+        <td></td>
+        <td><?=form_error('IdChapitre');?></td>
+    </tr>
+    <tr>
+      <td class="right"><label for="IdChapitre">Chapitre:</label></td>  
+      <td class="left">
+        <select name="IdChapitre" id="NomChapitre"> 
+        <?php
+          foreach( $chapitres as $chapitre ) {
+              $selected = $default['chapitre'] == $chapitre['id'] ? 'selected' : '' ;
+        ?>
+	      <option value='<?=$chapitre['id']; ?>' <?=$selected; ?>><?=$chapitre['num']; ?> : <?=$chapitre['nom']; ?></option>
+        <?php
+          }
+        ?>
+        </select>
+      </td>
+	</tr>
+    <tr>
+    </tr>
+  </table><br/>
+
+  <h4>Type de contenu :</h4>
+  <div class="center">
+    <p><?=form_error('Type');?></p>
+    <label><input type="radio" name="Type" <?=set_radio('Type', 'Resume'); ?> value="Resume"/>Résumé de cours</label>
+    <label><input type="radio" name="Type" <?=set_radio('Type', 'Complet'); ?> value="Complet"/>Cours complet</label><br/><br/>
+  </div>
+  <fieldset class="noborder">
+    <p><?=form_error('Cours');?></p>
+    <legend><h4>Chemin du contenu(1Mo max):</h4></legend>
+    <input type="file" name="Cours" />
+  </fieldset><br/>
+  <p><?=form_error('code');?></p>
+  <label for="code"><?=$captcha ?>?</label>
+  <input type="text" name="code" id="code" /><br/><br/><br/>
+  <div class="center"><input type="submit" name="cours_publier" value="  Publier  " /></div>
+</form>
+
+
+<?php
+  //Afficher la partie basse
+  $this->load->view('includes/partie_basse.php');
+?>
